cf56f3011aeb1621ae13c33e427d67c42e5632ac
adorableyou26 <adorableyou26@gmail.com>
2023-10-12 11:37:21 +0900

    commit: test

--- Lecture/src/main/java/org/example/Main.java
+++ Lecture/src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        String str = new String("test");
+        System.out.println(str);
+        HashMap<String, Objects> test = new HashMap<>();
+
     }
 }
