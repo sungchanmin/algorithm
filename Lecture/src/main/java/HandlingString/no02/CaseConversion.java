798d6c36b48773fe4f0c31f39c7c60e2973b7902
adorableyou26 <adorableyou26@gmail.com>
2023-10-12 21:34:58 +0900

    문제 02. 대소문자 변환

--- Lecture/src/main/java/HandlingString/no02/CaseConversion.java
+++ Lecture/src/main/java/HandlingString/no02/CaseConversion.java
@@ -3,6 +3,14 @@ package HandlingString.no02;
 import java.util.Scanner;
 
 public class CaseConversion {
+    public static void solution2 (String str) {
+        String answer = "";
+        for(char c : str.toCharArray()) {
+            answer += c >= 97 && c <=122 ? (char)(c - 32) : (char)(c + 32);
+        }
+
+        System.out.println(answer);
+    }
     public static void solution (String str) {
         String answer = "";
 
@@ -17,5 +25,6 @@ public class CaseConversion {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         solution(str);
+        solution2(str);
     }
 }
