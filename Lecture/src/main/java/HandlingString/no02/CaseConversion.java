59a55a68ac8beb7e5bf813cc0236986627a4f7f3
adorableyou26 <adorableyou26@gmail.com>
2023-10-12 20:36:32 +0900

    문제 02. 대소문자 변환

---
+++ Lecture/src/main/java/HandlingString/no02/CaseConversion.java
@@ -0,0 +1,21 @@
+package HandlingString.no02;
+
+import java.util.Scanner;
+
+public class CaseConversion {
+    public static void solution (String str) {
+        String answer = "";
+
+        for(char c : str.toCharArray()) {
+            answer += Character.isLowerCase(c) ? Character.toUpperCase(c) : Character.toLowerCase(c);
+        }
+
+        System.out.println(answer);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        solution(str);
+    }
+}
