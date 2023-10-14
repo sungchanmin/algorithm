01577b554c654c3d1bfbc957c773088b8241322d
adorableyou26 <adorableyou26@gmail.com>
2023-10-14 22:19:42 +0900

    문제 08. 회문 문자열(정규식 활용)

---
+++ Lecture/src/main/java/HandlingString/no08/PalindoromeString2.java
@@ -0,0 +1,31 @@
+package HandlingString.no08;
+
+import java.util.Scanner;
+
+public class PalindoromeString2 {
+    private static String solution(String str) {
+        String alphas = "";
+        char[] chars = str.toCharArray();
+
+        for(char c : chars) {
+            if(Character.isAlphabetic(c)) alphas += c;
+        }
+
+        if(!alphas.equalsIgnoreCase(new StringBuilder(alphas).reverse().toString())) return "NO";
+
+        return "YES";
+    }
+    private static String solution2(String str) {
+        str = str.toUpperCase();
+        str = str.replaceAll("[^A-Z]","");
+
+        if(!str.equals(new StringBuilder(str).reverse().toString())) return "NO";
+
+        return "YES";
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(solution(sc.nextLine()));
+        System.out.println(solution2(sc.nextLine()));
+    }
+}
