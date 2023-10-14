65373690e95f16ec8cbc5c8bf82c8f6430b5f1ee
adorableyou26 <adorableyou26@gmail.com>
2023-10-14 20:04:23 +0900

    문제 03. 문장 속 단어

---
+++ Lecture/src/main/java/HandlingString/no03/WordInSentence.java
@@ -0,0 +1,38 @@
+package HandlingString.no03;
+
+import java.util.Scanner;
+
+public class WordInSentence {
+    public static String solution(String str) {
+        String answer = "";
+        String[] words = str.split(" ");
+
+        for(String s : words) {
+            if(answer.length() < s.length()) answer = s;
+        }
+
+        return answer;
+    }
+
+    public static String soulution2(String str) {
+        String answer = "";
+        int pos;
+
+        while((pos = str.indexOf(" ")) != -1) {
+            String subStr = str.substring(0, pos);
+            if(answer.length() < subStr.length()) {
+                answer = subStr;
+            }
+            str = str.substring(pos + 1);
+        }
+
+        return str.length() > answer.length() ? str : answer;
+    }
+
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       String str = sc.nextLine();
+        System.out.println(solution(str));
+        System.out.println(soulution2(str));
+    }
+}
---
+++ Lecture/src/main/java/HandlingString/no04/FlipTheWord.java
@@ -0,0 +1,4 @@
+package HandlingString.no04;
+
+public class FlipTheWord {
+}
