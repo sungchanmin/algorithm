e9ed6f5f0afc43fad080b9ff0f801e1580ba1297
adorableyou26 <adorableyou26@gmail.com>
2023-10-14 22:20:01 +0900

    문제 09. 숫자만 추출

---
+++ Lecture/src/main/java/HandlingString/no09/ExtractingNumbersOnly.java
@@ -0,0 +1,26 @@
+package HandlingString.no09;
+
+import java.util.Scanner;
+
+public class ExtractingNumbersOnly {
+    private static int solution(String str) {
+        str = str.replaceAll("[^0-9]","");
+        return Integer.parseInt(str);
+    }
+
+    private static int solution2(String str) {
+        int answer = 0;
+
+        for(char x : str.toCharArray()) {
+            if(x >= 48 && x <= 57) answer = answer * 10 + (x-48);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(solution(sc.next()));
+        System.out.println(solution2(sc.next()));
+    }
+}
