4f14fd37884afd881c260960a2405df6bb2a3f71
chanmin <behonestway@gmail.com>
2023-10-27 15:23:25 +0900

    20. 등수 구하기

--- Lecture/src/main/java/chapter02/Array/no20/FindRank.java
+++ Lecture/src/main/java/chapter02/Array/no20/FindRank.java
@@ -1,4 +1,28 @@
 package chapter02.Array.no20;
 
+import java.util.Scanner;
+
 public class FindRank {
+    private static String solution(int n, String str) {
+        String answer = "";
+        String[] scores = str.split(" ");
+
+        for(String s : scores) {
+            int count = 1;
+
+            for(String c : scores) {
+                if(Integer.parseInt(s) < Integer.parseInt(c)) count++;
+            }
+
+            answer += count + " ";
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        String str = sc.nextLine();
+        System.out.println(solution(n, str));
+    }
 }
