e1d3f46ddf1f4f4bf4c35dcfd114b8ae208e9963
chanmin <behonestway@gmail.com>
2023-10-28 12:26:58 +0900

    27. 최대 매출(Sliding window)

--- Lecture/src/main/java/chapter03/TwoPointersSlidingWindow/no27/MaximumSales.java
+++ Lecture/src/main/java/chapter03/TwoPointersSlidingWindow/no27/MaximumSales.java
@@ -1,4 +1,33 @@
 package chapter03.TwoPointersSlidingWindow.no27;
 
+import java.util.Scanner;
+
 public class MaximumSales {
+    private static int solution(int n, int m, String[] strArr) {
+        int answer = 0, sum = 0;
+
+        for(int i=0; i<m; i++) {
+            sum += Integer.parseInt(strArr[i]);
+        }
+
+        answer = sum;
+
+        for(int i=m; i< n; i++) {
+            sum -= Integer.parseInt(strArr[i - m]);
+            sum += Integer.parseInt(strArr[i]);
+            answer = Math.max(sum, answer);
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] strArr = sc.nextLine().split(" ");
+
+        int n = Integer.parseInt(strArr[0]);
+        int m = Integer.parseInt(strArr[1]);
+        strArr = sc.nextLine().split(" ");
+
+        System.out.println(solution(n, m, strArr));
+    }
 }
