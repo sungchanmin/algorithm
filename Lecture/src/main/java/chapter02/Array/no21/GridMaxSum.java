c537983f54175240a6d9cc84d5551565f9f3ebb5
chanmin <behonestway@gmail.com>
2023-10-28 17:05:58 +0900

    21. 격자판 최대합

--- Lecture/src/main/java/chapter02/Array/no21/GridMaxSum.java
+++ Lecture/src/main/java/chapter02/Array/no21/GridMaxSum.java
@@ -7,13 +7,11 @@ public class GridMaxSum {
         int answer = 0;
         int[] sum = new int[2*n + 2];
 
-        // 가로 합 구하기
         for(int i=0; i<n; i++) {
-            for(String s : strings[i]) sum[i] += Integer.parseInt(s);
-
-            // 세로 합 구하기
             for(int j=0; j<n; j++) {
-                sum[n + j] += Integer.parseInt(strings[i][j]);
+                // 가로, 세로합 구하기
+                sum[i] += Integer.parseInt(strings[i][j]);
+                sum[n + i] += Integer.parseInt(strings[j][i]);
 
                 // 대각합 구하기
                 if(i == j) sum[2 * n] += Integer.parseInt(strings[i][j]);
@@ -24,7 +22,7 @@ public class GridMaxSum {
         }
 
         for(int i : sum) {
-            if(answer < i) answer = i;
+            answer = Math.max(i, answer);
         }
 
         return answer;
