dd3251c152a2f9f3933a583cdff0640ac8d5d288
chanmin <behonestway@gmail.com>
2023-10-28 18:00:00 +0900

    29. 연속된 자연수의 합(two pointers)

--- Lecture/src/main/java/chapter03/TwoPointersSlidingWindow/no29/SumConsecutiveNumbers.java
+++ Lecture/src/main/java/chapter03/TwoPointersSlidingWindow/no29/SumConsecutiveNumbers.java
@@ -4,28 +4,6 @@ import java.util.Scanner;
 
 public class SumConsecutiveNumbers {
     private static int solution(int n) {
-        int answer = 0, sum, r = 1, l = 1;
-
-        while(r < n) {
-            sum = 0;
-
-            for(int i=l; i<=r; i++) {
-                sum += i;
-            }
-
-            if(sum == n) {
-                answer++;
-                r++;
-            }
-            else if(sum < n) {
-                r++;
-            } else l++;
-        }
-
-        return answer;
-    }
-
-    private static int solution2(int n) {
         int answer = 0, sum = 0, r = 1, l = 1;
 
         while(r < n) {
