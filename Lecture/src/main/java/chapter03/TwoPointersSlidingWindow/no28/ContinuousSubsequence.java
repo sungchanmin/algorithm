f765853756e788f489af2f4468b9b04ec75dad85
chanmin <behonestway@gmail.com>
2023-10-28 13:32:16 +0900

    28. 연속 부분 수열(Sliding window)

--- Lecture/src/main/java/chapter03/TwoPointersSlidingWindow/no28/ContinuousSubsequence.java
+++ Lecture/src/main/java/chapter03/TwoPointersSlidingWindow/no28/ContinuousSubsequence.java
@@ -1,4 +1,36 @@
 package chapter03.TwoPointersSlidingWindow.no28;
 
+import java.util.Scanner;
+
 public class ContinuousSubsequence {
+    private static int solution(int n, int m, String[] strArr) {
+        int answer = 0, r = 0, l = 0;
+
+        while(r < n) {
+            int sum = 0;
+
+            for(int i=l; i<=r; i++) {
+                sum += Integer.parseInt(strArr[i]);
+            }
+
+            if(sum == m) {
+                answer++;
+                r++;
+            }
+            else if(sum < m) r++;
+            else l++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        String[] strArr = sc.nextLine().split(" ");
+        int n = Integer.parseInt(strArr[0]);
+        int m = Integer.parseInt(strArr[1]);
+        strArr = sc.nextLine().split(" ");
+
+        System.out.println(solution(n, m, strArr));
+    }
 }
