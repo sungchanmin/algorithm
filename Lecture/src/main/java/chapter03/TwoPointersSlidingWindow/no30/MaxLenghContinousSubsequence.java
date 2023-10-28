9eb2193446c4dbc99b7a512c356e82d046bb0bc4
chanmin <behonestway@gmail.com>
2023-10-28 16:01:49 +0900

    30. 최대 길이 연속 부분 수열

--- Lecture/src/main/java/chapter03/TwoPointersSlidingWindow/no30/MaxLenghContinousSubsequence.java
+++ Lecture/src/main/java/chapter03/TwoPointersSlidingWindow/no30/MaxLenghContinousSubsequence.java
@@ -1,4 +1,33 @@
 package chapter03.TwoPointersSlidingWindow.no30;
 
+import java.util.Scanner;
+
 public class MaxLenghContinousSubsequence {
+    private static int solution(int n, int m, String[] strArr) {
+        int answer = 0, l = 0, r = 0, zero = 0, len = 0;
+
+        while(r < n && l < n) {
+            if(zero <= m) {
+                answer = Math.max(answer, len);
+                if(strArr[r].equals("0")) zero++;
+                len++;
+                r++;
+            } else {
+                if(strArr[l].equals("0")) zero--;
+                len--;
+                l++;
+            }
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] strArr = sc.nextLine().split(" ");
+        int n = Integer.parseInt(strArr[0]);
+        int m = Integer.parseInt(strArr[1]);
+        strArr = sc.nextLine().split(" ");
+
+        System.out.println(solution(n, m, strArr));
+    }
 }
