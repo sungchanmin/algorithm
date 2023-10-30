3c936bb75be697135674a2dc97c30a615d70a17c
chanmin <behonestway@gmail.com>
2023-10-30 23:16:45 +0900

    32. 아나그램(해쉬)

--- Lecture/src/main/java/chapter04/HashMapTreeSet/no32/Anagram.java
+++ Lecture/src/main/java/chapter04/HashMapTreeSet/no32/Anagram.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Anagram {
-    private static boolean solution(String str1, String str2) {
+    private static String solution(String str1, String str2) {
         HashMap<Character, Integer> map = new HashMap<>();
 
         for(char c : str1.toCharArray()) {
@@ -12,16 +12,16 @@ public class Anagram {
         }
 
         for(char c : str2.toCharArray()) {
-            if(!map.containsKey(c) || map.get(c) <= 0) return false;
+            if(!map.containsKey(c) || map.get(c) <= 0) return "NO";
             else map.merge(c, -1, Integer::sum);
         }
 
-        return true;
+        return "YES";
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str1 = sc.nextLine();
         String str2 = sc.nextLine();
-        System.out.println(solution(str1, str2) ? "YES" : "NO");
+        System.out.println(solution(str1, str2));
     }
 }
