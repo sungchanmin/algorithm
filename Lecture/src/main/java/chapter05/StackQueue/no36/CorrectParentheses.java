79b5914bbb28891ae326d1a2c66f3ddec2f9530b
chanmin <behonestway@gmail.com>
2023-10-30 23:01:40 +0900

    36. 올바른 괄호

--- Lecture/src/main/java/chapter05/StackQueue/no36/CorrectParentheses.java
+++ Lecture/src/main/java/chapter05/StackQueue/no36/CorrectParentheses.java
@@ -1,7 +1,24 @@
 package chapter05.StackQueue.no36;
 
-public class CorrectParentheses {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class CorrectParentheses {
+    private static String solution(String[] strArr) {
+        int n = 0;
+
+        if(!strArr[0].equals("(") || !strArr[strArr.length - 1].equals(")"))
+            return "NO";
+
+        for(String s : strArr) {
+            if(s.equals("(")) n++;
+            else n--;
+        }
+
+        return n == 0 ? "YES" : "NO";
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] strArr = sc.nextLine().split("");
+        System.out.println(solution(strArr));
     }
 }
