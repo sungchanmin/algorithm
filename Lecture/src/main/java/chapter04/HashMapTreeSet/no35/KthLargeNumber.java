4adb0589dba347e2207e738d38dcc2d1aea3c16b
chanmin <behonestway@gmail.com>
2023-10-30 23:49:10 +0900

    35. K번째 큰 수(TreeSet)

--- Lecture/src/main/java/chapter04/HashMapTreeSet/no35/KthLargeNumber.java
+++ Lecture/src/main/java/chapter04/HashMapTreeSet/no35/KthLargeNumber.java
@@ -1,12 +1,26 @@
 package chapter04.HashMapTreeSet.no35;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 public class KthLargeNumber {
     private static int solution(int n, int m, Integer[] arr) {
+        TreeSet<Integer> tree = new TreeSet<>(Collections.reverseOrder());
+
+        for(int i=0; i<n; i++) {
+            for(int j=i+1; j<n; j++) {
+                for(int k=j+1; k<n; k++) {
+                    tree.add(arr[i] + arr[j] + arr[k]);
+                }
+            }
+        }
+
+        for(Integer i : tree) {
+            if(--m == 0) return i;
+        }
+
+        return -1;
+    }
+    private static int solution2(int n, int m, Integer[] arr) {
         ArrayList<Integer> list = new ArrayList<>();
         HashMap<Integer, Integer> dupCheck = new HashMap<>();
 
