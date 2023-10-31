d64f813028e8b32a1f918fdc741e69c12f3d3ecb
chanmin <behonestway@gmail.com>
2023-10-31 18:39:40 +0900

    38. 크레인 인형뽑기(카카오)

--- Lecture/src/main/java/chapter05/StackQueue/no38/CraneDollPulling.java
+++ Lecture/src/main/java/chapter05/StackQueue/no38/CraneDollPulling.java
@@ -1,4 +1,52 @@
 package chapter05.StackQueue.no38;
 
+import java.util.*;
+
 public class CraneDollPulling {
+    private static int solution(List<Queue<Integer>> board, int[] moves) {
+        int answer = 0;
+        Stack<Integer> bucket = new Stack<>();
+
+        for(int i : moves) {
+            int doll = 0;
+
+            if(!board.get(i-1).isEmpty()) {
+                doll = board.get(i-1).poll();
+            }
+
+            if(doll != 0) {
+                if(bucket.empty() || bucket.peek() != doll) bucket.push(doll);
+                else {
+                    bucket.pop();
+                    answer += 2;
+                }
+            }
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        List<Queue<Integer>> board = new ArrayList<>();
+
+        for(int i=0; i<n; i++) board.add(new LinkedList<>());
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                int input = sc.nextInt();
+                if(input !=0) {
+                    board.get(j).add(input);
+                }
+            }
+        }
+
+        int m = sc.nextInt();
+        int[] moves = new int[m];
+
+        for(int i=0; i<m; i++) moves[i] = sc.nextInt();
+
+        System.out.println(solution(board, moves));
+    }
 }
