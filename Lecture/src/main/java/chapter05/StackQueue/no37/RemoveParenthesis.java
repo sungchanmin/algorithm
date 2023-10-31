ea611f9483f70ebef60679a50f906d2a583c27ed
chanmin <behonestway@gmail.com>
2023-10-31 18:05:44 +0900

    37. 괄호 문자 제거

--- Lecture/src/main/java/chapter05/StackQueue/no37/RemoveParenthesis.java
+++ Lecture/src/main/java/chapter05/StackQueue/no37/RemoveParenthesis.java
@@ -1,4 +1,24 @@
 package chapter05.StackQueue.no37;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class RemoveParenthesis {
+    private static String solution(String[] strArr) {
+        StringBuilder sb = new StringBuilder();
+        Stack<String> stack = new Stack<>();
+
+        for(String s : strArr) {
+            if(s.equals("(")) stack.push(s);
+            else if(s.equals(")")) stack.pop();
+            else if(stack.size() == 0) sb.append(s);
+        }
+
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] strArr = sc.nextLine().split("");
+        System.out.println(solution(strArr));
+    }
 }
