b2f9729235a8bb7e9fe25a3637013c08a21a7129
chanmin <behonestway@gmail.com>
2023-11-01 11:50:37 +0900

    44. 선택 정렬

--- Lecture/src/main/java/chapter06/SortingSearching/no44/SelectionSort.java
+++ Lecture/src/main/java/chapter06/SortingSearching/no44/SelectionSort.java
@@ -1,4 +1,36 @@
 package chapter06.SortingSearching.no44;
 
+import java.util.Scanner;
+
 public class SelectionSort {
+    private static int[] solution(int[] arr) {
+        int min, tmp, index;
+
+        for(int i=0; i<arr.length-1; i++) {
+            min = arr[i];
+            index = i;
+            for(int j=i+1; j<arr.length; j++) {
+                if(min > arr[j]) {
+                    min = arr[j];
+                    index = j;
+                }
+            }
+            tmp = arr[i];
+            arr[i] = arr[index];
+            arr[index] = tmp;
+        }
+
+        return arr;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0; i<n; i++) arr[i] = sc.nextInt();
+
+        arr = solution(arr);
+
+        for(int i : arr) System.out.print(i + " ");
+    }
 }
