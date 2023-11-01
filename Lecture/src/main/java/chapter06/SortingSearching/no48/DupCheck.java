f7e5e45900c6be8bfbb21245190d4e9d0ca91394
chanmin <behonestway@gmail.com>
2023-11-01 15:55:43 +0900

    48. 중복 확인

--- Lecture/src/main/java/chapter06/SortingSearching/no48/DupCheck.java
+++ Lecture/src/main/java/chapter06/SortingSearching/no48/DupCheck.java
@@ -1,7 +1,25 @@
 package chapter06.SortingSearching.no48;
 
-public class DupCheck {
-    public static void main(String[] args) {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class DupCheck {
+    private static String solution(int n, int[] arr) {
+        Arrays.sort(arr);
+
+        for(int i=0; i<arr.length-1; i++) {
+            if(arr[i] == arr[i+1]) return "D";
+        }
+
+        return "U";
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0; i<n; i++) arr[i] = sc.nextInt();
+
+        System.out.println(solution(n, arr));
     }
 }
