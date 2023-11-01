484b05174711483c761ef2dacdb47ac963d8f424
chanmin <behonestway@gmail.com>
2023-11-01 12:08:26 +0900

    45. 버블 정렬

--- Lecture/src/main/java/chapter06/SortingSearching/no45/BubbleSort.java
+++ Lecture/src/main/java/chapter06/SortingSearching/no45/BubbleSort.java
@@ -1,7 +1,31 @@
 package chapter06.SortingSearching.no45;
 
-public class BubbleSort {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class BubbleSort {
+    private static int[] solution(int[] arr) {
+        int tmp;
+
+        for(int i=0; i<arr.length-1; i++) {
+            for(int j=0; j<arr.length-1; j++) {
+                if(arr[j] > arr[j+1]) {
+                    tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0; i<n; i++) arr[i] = sc.nextInt();
+
+        arr = solution(arr);
+
+        for(int i : arr) System.out.print(i + " ");
     }
 }
