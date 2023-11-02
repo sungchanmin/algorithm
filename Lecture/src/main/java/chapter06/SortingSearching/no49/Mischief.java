ab89131fd49b9a079da7967966cf3b92e1674e26
chanmin <behonestway@gmail.com>
2023-11-02 20:50:05 +0900

    49. 장난꾸러기

--- Lecture/src/main/java/chapter06/SortingSearching/no49/Mischief.java
+++ Lecture/src/main/java/chapter06/SortingSearching/no49/Mischief.java
@@ -1,4 +1,34 @@
 package chapter06.SortingSearching.no49;
 
+import java.util.Scanner;
+
 public class Mischief {
+    private static String solution(int[] arr) {
+        int c = 0, p = 0;
+
+        for(int i=arr.length-1; i>1; i--) {
+            if(arr[i] < arr[i - 1]) {
+                p = i + 1;
+                break;
+            }
+        }
+
+        for(int i=0; i<p; i++ ) {
+            if(arr[i] > arr[p-1]) {
+                c = i + 1;
+                break;
+            }
+        }
+
+        return c + " " + p;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+        System.out.println(solution(arr));
+    }
 }
