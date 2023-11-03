021ade97cd05909bb1e975c8618dd5cb45f571dd
chanmin <behonestway@gmail.com>
2023-11-03 13:04:52 +0900

    49. 장난꾸러기

--- Lecture/src/main/java/chapter06/SortingSearching/no49/Mischief.java
+++ Lecture/src/main/java/chapter06/SortingSearching/no49/Mischief.java
@@ -1,26 +1,39 @@
 package chapter06.SortingSearching.no49;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Mischief {
+//    private static String solution(int[] arr) {
+//        int c = 0, p = 0;
+//
+//        for(int i=arr.length-1; i>1; i--) {
+//            if(arr[i] < arr[i - 1]) {
+//                p = i + 1;
+//                break;
+//            }
+//        }
+//
+//        for(int i=0; i<p; i++ ) {
+//            if(arr[i] > arr[p-1]) {
+//                c = i + 1;
+//                break;
+//            }
+//        }
+//
+//        return c + " " + p;
+//    }
+
     private static String solution(int[] arr) {
-        int c = 0, p = 0;
+        String answer = "";
+        int[] compare = arr.clone();
+        Arrays.sort(compare);
 
-        for(int i=arr.length-1; i>1; i--) {
-            if(arr[i] < arr[i - 1]) {
-                p = i + 1;
-                break;
-            }
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] != compare[i]) answer += i + 1 + " ";
         }
 
-        for(int i=0; i<p; i++ ) {
-            if(arr[i] > arr[p-1]) {
-                c = i + 1;
-                break;
-            }
-        }
-
-        return c + " " + p;
+        return answer;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
