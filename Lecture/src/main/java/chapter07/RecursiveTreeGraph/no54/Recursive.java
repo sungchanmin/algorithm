04a19993618b948fcc712bd785be3edb18faee3d
chanmin <behonestway@gmail.com>
2023-11-03 21:17:30 +0900

    54. 재귀 함수(스택 프레임)

--- Lecture/src/main/java/chapter07/RecursiveTreeGraph/no54/Recursive.java
+++ Lecture/src/main/java/chapter07/RecursiveTreeGraph/no54/Recursive.java
@@ -1,7 +1,17 @@
 package chapter07.RecursiveTreeGraph.no54;
 
-public class Recursive {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Recursive {
+    private static void DFS(int n) {
+        if(n==0) return;
+        else {
+            System.out.print(n + " ");
+            DFS(n-1);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        DFS(sc.nextInt());
     }
 }
