05dc7dafd6e6adbbe0fa06d7b97a68355964b189
chanmin <behonestway@gmail.com>
2023-11-03 22:01:32 +0900

    57. 피보나치 재귀

--- Lecture/src/main/java/chapter07/RecursiveTreeGraph/no57/Fibonacci.java
+++ Lecture/src/main/java/chapter07/RecursiveTreeGraph/no57/Fibonacci.java
@@ -5,9 +5,7 @@ import java.util.Scanner;
 public class Fibonacci {
     private static int DFS(int n) {
         if(n <= 2) return 1;
-        else {
-            return DFS(n - 1) + DFS(n - 2);
-        }
+        else return DFS(n - 1) + DFS(n - 2);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
