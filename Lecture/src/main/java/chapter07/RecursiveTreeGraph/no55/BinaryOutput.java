43fd7ddc182532c43d047f81cda15700746422eb
chanmin <behonestway@gmail.com>
2023-11-03 21:17:42 +0900

    55. 이진수 출력(재귀)

--- Lecture/src/main/java/chapter07/RecursiveTreeGraph/no55/BinaryOutput.java
+++ Lecture/src/main/java/chapter07/RecursiveTreeGraph/no55/BinaryOutput.java
@@ -1,7 +1,17 @@
 package chapter07.RecursiveTreeGraph.no55;
 
-public class BinaryOutput {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class BinaryOutput {
+    private static void DFS(int n) {
+        if(n == 0) return;
+
+        DFS(n/2);
+        if(n % 2 == 1) System.out.print(1);
+        else System.out.print(0);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        DFS(sc.nextInt());
     }
 }
