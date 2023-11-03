5625b7f3927f6169f2d819b24fd2bf2de51b7518
chanmin <behonestway@gmail.com>
2023-11-03 13:35:51 +0900

    51. 뮤직 비디오(결정 알고리즘)

--- Lecture/src/main/java/chapter06/SortingSearching/no52/MusicVideo2.java
+++ Lecture/src/main/java/chapter06/SortingSearching/no52/MusicVideo2.java
@@ -1,10 +1,38 @@
 package chapter06.SortingSearching.no52;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MusicVideo2 {
+    private static int count(int[] arr, int capacity) {
+        int cnt = 1;
+        int sum = 0;
+
+        for(int i : arr) {
+            if(sum + i > capacity) {
+                cnt++;
+                sum = i;
+            } else sum += i;
+        }
+
+        return cnt;
+    }
+
     private static int solution(int m, int[] arr) {
-        return 0;
+        int answer = 0;
+        int lt = Arrays.stream(arr).max().getAsInt();
+        int rt = Arrays.stream(arr).sum();
+
+        while(lt <= rt) {
+            int mid = (lt + rt) / 2;
+
+            if(count(arr, mid) <= m) {
+                answer = mid;
+                rt = mid - 1;
+            } else lt = mid + 1;
+        }
+
+        return answer;
     }
 
     public static void main(String[] args) {
