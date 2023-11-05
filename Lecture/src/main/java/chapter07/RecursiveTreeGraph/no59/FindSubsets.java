1a6e921b31f6c7508d408e58bf412c9f83046be2
chanmin <behonestway@gmail.com>
2023-11-05 21:29:48 +0900

    59. 부분 집합 구하기(DFS)

--- Lecture/src/main/java/chapter07/RecursiveTreeGraph/no59/FindSubsets.java
+++ Lecture/src/main/java/chapter07/RecursiveTreeGraph/no59/FindSubsets.java
@@ -1,7 +1,32 @@
 package chapter07.RecursiveTreeGraph.no59;
 
-public class FindSubsets {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class FindSubsets {
+    private static int[] ch;
+    private static void DFS(int n) {
+        if(n == ch.length) {
+            String str = "";
+
+            for(int i=0; i<ch.length; i++) {
+                if(ch[i] == 1) str+=i;
+            }
+
+            if(!str.equals("")) System.out.println(str);
+
+        } else {
+            ch[n] = 1;
+            DFS(n+1);
+            ch[n] = 0;
+            DFS(n+1);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ch = new int[n + 1];
+        DFS(1);
     }
 }
