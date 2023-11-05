1fc00d8a87e0dae95f4258fad3610b701e580fb2
chanmin <behonestway@gmail.com>
2023-11-05 21:00:07 +0900

    58. 이진 트리 순회

--- Lecture/src/main/java/chapter07/RecursiveTreeGraph/no58/BinaryTreeCircuit.java
+++ Lecture/src/main/java/chapter07/RecursiveTreeGraph/no58/BinaryTreeCircuit.java
@@ -1,7 +1,53 @@
 package chapter07.RecursiveTreeGraph.no58;
 
 public class BinaryTreeCircuit {
-    public static void main(String[] args) {
+    public static class Node {
+        int data;
+        Node lt, rt;
 
+        public Node(int val) {
+            data = val;
+            lt = rt = null;
+        }
+    }
+
+    public static void preorderTraversal(Node node) {
+        if(node == null) return;
+        System.out.print(node.data + " ");
+        preorderTraversal(node.lt);
+        preorderTraversal(node.rt);
+    }
+
+    public static void inorderTraversal(Node node) {
+        if(node == null) return;
+        inorderTraversal(node.lt);
+        System.out.print(node.data + " ");
+        inorderTraversal(node.rt);
+    }
+
+    public static void postorderTraversal(Node node) {
+        if(node == null) return;
+        postorderTraversal(node.lt);
+        postorderTraversal(node.rt);
+        System.out.print(node.data + " ");
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.lt = new Node(2);
+        root.rt = new Node(3);
+        root.lt.lt = new Node(4);
+        root.lt.rt = new Node(5);
+        root.rt.lt = new Node(6);
+        root.rt.rt = new Node(7);
+
+        System.out.print("전위 순회 출력 : ");
+        preorderTraversal(root);
+
+        System.out.print("\n중위 순회 출력 : ");
+        inorderTraversal(root);
+
+        System.out.print("\n후위 순회 출력 : ");
+        postorderTraversal(root);
     }
 }
