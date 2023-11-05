42677c95628bc5a389ce9bfa294f3c22e97d6fea
chanmin <behonestway@gmail.com>
2023-11-05 21:48:42 +0900

    60. 이진 트리 순회(넓이 우선 탐색 : 레벨 탐색)

--- Lecture/src/main/java/chapter07/RecursiveTreeGraph/no60/BinaryTreeLevelTraversal.java
+++ Lecture/src/main/java/chapter07/RecursiveTreeGraph/no60/BinaryTreeLevelTraversal.java
@@ -1,7 +1,46 @@
 package chapter07.RecursiveTreeGraph.no60;
 
-public class BinaryTreeLevelTraversal {
-    public static void main(String[] args) {
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class BinaryTreeLevelTraversal {
+    private static class Node {
+        int data;
+        Node rt;
+        Node lt;
+
+        public Node(int data) {
+            this.data = data;
+            rt = lt = null;
+        }
+    }
+
+    private static void BFS(Node node) {
+        Queue<Node> Q = new LinkedList<>();
+        Q.offer(node);
+
+        while(!Q.isEmpty()) {
+            int len = Q.size();
+
+            for(int i=0; i<len; i++) {
+                Node n = Q.poll();
+                System.out.print(n.data + " ");
+
+                if(n.lt != null) Q.add(n.lt);
+                if(n.lt != null) Q.add(n.rt);
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.lt = new Node(2);
+        root.rt = new Node(3);
+        root.lt.lt = new Node(4);
+        root.lt.rt = new Node(5);
+        root.rt.lt = new Node(6);
+        root.rt.rt = new Node(7);
+
+        BFS(root);
     }
 }
