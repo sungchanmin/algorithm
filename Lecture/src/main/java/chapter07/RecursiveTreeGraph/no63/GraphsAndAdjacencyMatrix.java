d76530dc775d83e1b38a013eec403c30f126af28
chanmin <behonestway@gmail.com>
2023-11-06 19:10:20 +0900

    62. 그래프와 인접행렬

--- Lecture/src/main/java/chapter07/RecursiveTreeGraph/no63/GraphsAndAdjacencyMatrix.java
+++ Lecture/src/main/java/chapter07/RecursiveTreeGraph/no63/GraphsAndAdjacencyMatrix.java
@@ -1,7 +1,39 @@
 package chapter07.RecursiveTreeGraph.no63;
 
 public class GraphsAndAdjacencyMatrix {
-    public static void main(String[] args) {
+    public static void undirectionalGraph() {
+        int[][] graph = new int[6][6];
 
+        graph[1][2] = graph[2][1] = 1;
+        graph[1][3] = graph[3][1] = 1;
+        graph[2][4] = graph[4][2] = 1;
+        graph[2][5] = graph[5][2] = 1;
+        graph[3][4] = graph[4][3] = 1;
+    }
+
+    public static void directionalGraph() {
+        int[][] graph = new int[6][6];
+
+        graph[1][2] = 1;
+        graph[1][3] = 1;
+        graph[2][5] = 1;
+        graph[3][4] = 1;
+        graph[4][2] = 1;
+    }
+
+    public static void weightedOrientationGraph() {
+        int[][] graph = new int[6][6];
+
+        graph[1][2] = 2;
+        graph[1][3] = 4;
+        graph[2][5] = 5;
+        graph[3][4] = 5;
+        graph[4][2] = 2;
+    }
+
+    public static void main(String[] args) {
+        undirectionalGraph();
+        directionalGraph();
+        weightedOrientationGraph();
     }
 }
