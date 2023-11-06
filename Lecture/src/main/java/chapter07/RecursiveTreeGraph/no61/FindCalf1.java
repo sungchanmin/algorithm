3e2211e99e4281b6840faddda6ddd71a4c3c46e5
chanmin <behonestway@gmail.com>
2023-11-06 16:16:11 +0900

    61. 송아지 찾기1(BFS)

--- Lecture/src/main/java/chapter07/RecursiveTreeGraph/no61/FindCalf1.java
+++ Lecture/src/main/java/chapter07/RecursiveTreeGraph/no61/FindCalf1.java
@@ -1,7 +1,35 @@
 package chapter07.RecursiveTreeGraph.no61;
 
-public class FindCalf1 {
-    public static void main(String[] args) {
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
+public class FindCalf1 {
+    private static int solution(int n, int m) {
+        int answer = 0;
+        Queue<Integer> Q = new LinkedList<>();
+        Q.add(n);
+
+        while(true) {
+            int len = Q.size();
+
+            for(int i=0; i<len; i++) {
+                int x = Q.poll();
+                if(x == m) return answer;
+
+                if(x < m - 5) Q.add(x + 5);
+                else if(x < m) {
+                    Q.add(x + 5);
+                    Q.add(x + 1);
+                } else Q.add(x - 1);
+            }
+            answer++;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        System.out.println(solution(n, m));
     }
 }
