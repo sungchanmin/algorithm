1d8dca54f06c98760a85d78cc14aa43683e40cc9
chanmin <behonestway@gmail.com>
2023-11-06 16:56:09 +0900

    62. Tree 말단 노드까지의 가장 짧은 경로(DFS)

--- Lecture/src/main/java/chapter07/RecursiveTreeGraph/no62/ShortestPathToTreeEndNode_DFS.java
+++ Lecture/src/main/java/chapter07/RecursiveTreeGraph/no62/ShortestPathToTreeEndNode_DFS.java
@@ -1,8 +1,5 @@
 package chapter07.RecursiveTreeGraph.no62;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class ShortestPathToTreeEndNode_DFS {
     private static class Node {
         int data;
@@ -15,23 +12,12 @@ public class ShortestPathToTreeEndNode_DFS {
         }
     }
 
-    private static int BFS(Node node){
-        int answer = 0;
-        Queue<Node> Q = new LinkedList<>();
-        Q.add(node);
-
-        while(!Q.isEmpty()) {
-            int len = Q.size();
-            for(int i=0; i<len; i++) {
-                Node n = Q.poll();
-                if(n.lt == null || n.rt == null) return answer;
-                if(n.lt != null) Q.add(n.lt);
-                if(n.rt != null) Q.add(n.rt);
-            }
-            answer++;
-        }
-        return answer;
+    private static int DFS(int level, Node node) {
+        if(node.lt == null && node.rt == null) return level;
+        return Math.min(DFS(level + 1, node.lt), DFS(level + 1, node.rt));
     }
+
+
     public static void main(String[] args) {
         Node root = new Node(1);
         root.lt = new Node(2);
@@ -39,12 +25,7 @@ public class ShortestPathToTreeEndNode_DFS {
         root.lt.lt = new Node(4);
         root.lt.rt = new Node(5);
 
-        System.out.println(BFS(root));
+        System.out.println(DFS(0, root));
     }
 
-    public static class ShortestPathToTreeEndNode2 {
-        public static void main(String[] args) {
-
-        }
-    }
 }
