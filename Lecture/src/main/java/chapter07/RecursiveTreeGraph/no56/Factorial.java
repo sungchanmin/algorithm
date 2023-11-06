7c1ce89d60dac386a568501915ce1cd7b01bc731
chanmin <behonestway@gmail.com>
2023-11-06 13:18:13 +0900

    56. 팩토리얼

--- Lecture/src/main/java/chapter07/RecursiveTreeGraph/no56/Factorial.java
+++ Lecture/src/main/java/chapter07/RecursiveTreeGraph/no56/Factorial.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Factorial {
     private static int DFS(int n) {
         if(n == 1) return 1;
-        return (n *= DFS(n-1));
+        return n * DFS(n-1);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
