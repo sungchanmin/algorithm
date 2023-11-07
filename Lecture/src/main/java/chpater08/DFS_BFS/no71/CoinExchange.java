2398d83a943133bb04b97089d7abddf100b2dcab
chanmin <behonestway@gmail.com>
2023-11-07 21:18:10 +0900

    71. 동전 교환

--- Lecture/src/main/java/chpater08/DFS_BFS/no71/CoinExchange.java
+++ Lecture/src/main/java/chpater08/DFS_BFS/no71/CoinExchange.java
@@ -1,7 +1,45 @@
 package chpater08.DFS_BFS.no71;
 
-public class CoinExchange {
-    public static void main(String[] args) {
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
+public class CoinExchange {
+    private static int change;
+    private static int BFS(int[] coins) {
+        int answer = 0;
+        int maxCoins = Arrays.stream(coins).max().getAsInt();
+        Queue<Integer> Q = new LinkedList<>();
+        Q.add(0);
+
+        while(!Q.isEmpty()) {
+            int len = Q.size();
+            for(int i=0; i<len; i++) {
+                int sum = Q.poll();
+                if(change - maxCoins > sum) {
+                    Q.add(sum + maxCoins);
+                } else {
+                    for(int j=0; j<coins.length; j++) {
+                        if(sum + coins[j] == change) return ++answer;
+                        if(sum + coins[j] < change) Q.add(sum + coins[j]);
+                    }
+                }
+            }
+            answer++;
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] coins = new int[n];
+
+        for(int i=0; i<n; i++) coins[i] = sc.nextInt();
+
+        change = sc.nextInt();
+
+        System.out.println(BFS(coins));
     }
 }
