184e8a6f36c9ab4aa12c32fa3d9edf43144a6c22
chanmin <behonestway@gmail.com>
2023-11-07 13:48:10 +0900

    67. 합이 같은 부분집합

--- Lecture/src/main/java/chpater08/DFS_BFS/no67/SubsetWithEqualSum.java
+++ Lecture/src/main/java/chpater08/DFS_BFS/no67/SubsetWithEqualSum.java
@@ -1,7 +1,41 @@
 package chpater08.DFS_BFS.no67;
 
-public class SubsetWithEqualSum {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class SubsetWithEqualSum {
+    private static int[] use;
+    private static int DFS(int[] arr, int i) {
+        if(i >= arr.length) return 0;
+        int sum1 = 0;
+        int sum2 = 0;
+
+        if(i > 0) {
+            for (int k = 0; k < arr.length; k++) {
+                if (use[k] == 0) sum1 += arr[k];
+                if (use[k] == 1) sum2 += arr[k];
+            }
+        }
+        if(sum1 == sum2 && i > 0) return 1;
+
+        use[i] = 1;
+        int a = DFS(arr, i+1);
+        use[i] = 0;
+        int b = DFS(arr, i+1);
+
+        return a + b;
+    }
+    private static String solution(int[] arr) {
+        if(DFS(arr, 0) > 0) return "YES";
+        return "NO";
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        use = new int[n];
+
+        for(int i=0; i<n; i++) arr[i] = sc.nextInt();
+
+        System.out.println(solution(arr));
     }
 }
