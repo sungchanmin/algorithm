25b1040e5a1eb7a447d7331e7d0e3abced385ae2
chanmin <behonestway@gmail.com>
2023-11-07 21:18:24 +0900

    72. 순열 구하기

--- Lecture/src/main/java/chpater08/DFS_BFS/no72/SavingPermutations.java
+++ Lecture/src/main/java/chpater08/DFS_BFS/no72/SavingPermutations.java
@@ -1,7 +1,33 @@
 package chpater08.DFS_BFS.no72;
 
-public class SavingPermutations {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class SavingPermutations {
+    private static int[] ch;
+    private static void DFS(int m, int[]arr, String s) {
+        for(int i=0; i< arr.length; i++) {
+            if(ch[i] == 0) {
+                String str = s;
+                str += arr[i] + " ";
+
+                if (m == 1) System.out.println(str);
+                else {
+                    ch[i] = 1;
+                    DFS(m-1, arr, str);
+                    ch[i] = 0;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        ch = new int[n];
+
+        for(int i=0; i<n; i++) arr[i] = sc.nextInt();
+
+        DFS(m, arr, "");
     }
 }
