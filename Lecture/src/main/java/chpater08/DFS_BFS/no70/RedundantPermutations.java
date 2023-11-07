c2681e08dff6a47c5f49c73b78b5d68ff1fa874c
chanmin <behonestway@gmail.com>
2023-11-07 14:59:54 +0900

    70. 중복 순열 구하기

--- Lecture/src/main/java/chpater08/DFS_BFS/no70/RedundantPermutations.java
+++ Lecture/src/main/java/chpater08/DFS_BFS/no70/RedundantPermutations.java
@@ -1,7 +1,21 @@
 package chpater08.DFS_BFS.no70;
 
-public class RedundantPermutations {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class RedundantPermutations {
+    private static String DFS(int n, int m, String s) {
+        for(int i=1; i<=n; i++) {
+            String str = s;
+            str += i + " ";
+            if(m == 1) System.out.println(str);
+            else DFS(n, m-1, str);
+        }
+        return "";
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        DFS(n, m, "");
     }
 }
