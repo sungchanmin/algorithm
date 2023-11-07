a3faf6a233a07a2764d3ae14ef134c6a44d5500a
chanmin <behonestway@gmail.com>
2023-11-07 14:49:55 +0900

    69. 최대 점수 구하기ㅏ(DFS)

--- Lecture/src/main/java/chpater08/DFS_BFS/no69/GetMaxScore.java
+++ Lecture/src/main/java/chpater08/DFS_BFS/no69/GetMaxScore.java
@@ -1,7 +1,53 @@
 package chpater08.DFS_BFS.no69;
 
-public class GetMaxScore {
-    public static void main(String[] args) {
+import java.util.*;
 
+public class GetMaxScore {
+    private static int m, maxScore = 0;
+    private static int[] ch;
+    private static class P {
+        int score;
+        int time;
+
+        public P(int score, int time) {
+            this.score = score;
+            this.time = time;
+        }
+    }
+    private static void DFS(int n,  List<P> list) {
+        if(n >= list.size()) return;
+        int sum = 0;
+        int time = 0;
+
+        for(int i=0; i<list.size(); i++) {
+            if(ch[i] == 0) {
+                P p = list.get(i);
+                if(time + p.time > m) break;
+                time+= p.time;
+                sum+= p.score;
+            }
+        }
+
+        maxScore = Math.max(maxScore, sum);
+
+        ch[n] = 1;
+        DFS(n+1, list);
+        ch[n] = 0;
+        DFS(n+1, list);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        m = sc.nextInt();
+        List<P> list = new ArrayList<>();
+        ch = new int[n];
+
+        for(int i=0; i<n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            list.add(new P(a, b));
+        }
+        DFS(0, list);
+        System.out.println(maxScore);
     }
 }
