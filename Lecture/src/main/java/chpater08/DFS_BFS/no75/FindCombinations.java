d6f28dac4e2eecf746d17c0a5968b93aa6e43094
chanmin <behonestway@gmail.com>
2023-11-08 13:27:16 +0900

    75. 조합 구하기

--- Lecture/src/main/java/chpater08/DFS_BFS/no75/FindCombinations.java
+++ Lecture/src/main/java/chpater08/DFS_BFS/no75/FindCombinations.java
@@ -1,7 +1,25 @@
 package chpater08.DFS_BFS.no75;
 
-public class FindCombinations {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class FindCombinations {
+    private static int n, m;
+    private static void DFS(int i, int j, String str) {
+        if(i == m) {
+            System.out.println(str);
+            return;
+        }
+
+        for(int k=j; k<=n; k++) {
+            String marbles = str;
+            marbles += k + " ";
+            DFS(i+1, k+1 , marbles);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        DFS(0 , 1, "");
     }
 }
