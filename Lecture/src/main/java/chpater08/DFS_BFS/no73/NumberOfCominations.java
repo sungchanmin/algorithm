8b5409aaa63d4e39ef6cfdbbc2c4687fecc08948
chanmin <behonestway@gmail.com>
2023-11-08 10:35:28 +0900

    73. 조합의 경우 수(메모이제이션)

--- Lecture/src/main/java/chpater08/DFS_BFS/no73/NumberOfCominations.java
+++ Lecture/src/main/java/chpater08/DFS_BFS/no73/NumberOfCominations.java
@@ -1,7 +1,23 @@
 package chpater08.DFS_BFS.no73;
 
+import java.util.Scanner;
+
 public class NumberOfCominations {
+    private static int C[][];
+
+    private static int DFS(int n, int r) {
+        if(n == r) return 1;
+        if(r == 1) return n;
+        if(C[n][r] == 0) C[n][r] = DFS(n-1, r-1) + DFS(n-1 , r);
+        return C[n][r];
+    }
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int r = sc.nextInt();
+        C = new int[n+1][r+1];
+
+        System.out.println(DFS(n , r));
 
     }
 }
