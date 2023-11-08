e0d688c3daf4d0320571673e9232d94f14b8462b
chanmin <behonestway@gmail.com>
2023-11-08 12:07:52 +0900

    74. 수열 추측하기

--- Lecture/src/main/java/chpater08/DFS_BFS/no74/GuessTheSequence.java
+++ Lecture/src/main/java/chpater08/DFS_BFS/no74/GuessTheSequence.java
@@ -1,7 +1,50 @@
 package chpater08.DFS_BFS.no74;
 
-public class GuessTheSequence {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class GuessTheSequence {
+    private static int n, m, C[][], nums[], coms[], answer[];
+
+    private static int combination(int n, int r) {
+        if(n == r || r == 0) return 1;
+        if(r == 1) return n;
+        if(C[n][r] == 0) C[n][r] = combination(n-1, r-1) + combination(n-1 , r);
+        return C[n][r];
+    }
+    private static boolean DFS(int L) {
+        if (L == n) {
+            int sum = 0;
+            for(int i=0; i<n; i++) sum += coms[i] * answer[i];
+            if(sum == m) return true;
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == 0) {
+                    answer[L] = i + 1;
+                    nums[i] = 1;
+                    if(DFS(L+1)) return true;
+                    nums[i] = 0;
+                }
+            }
+        }
+        return false;
+    }
+    private static void solution() {
+        nums = new int[n];
+        coms = new int[n];
+        answer = new int[n];
+
+        for(int i=0; i<=n-1; i++) {
+            coms[i] = combination(n-1, i);
+        }
+        DFS(0);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        C = new int[n+1][n+1];
+        solution();
+
+        for(int i : answer) System.out.print(i + " ");
     }
 }
