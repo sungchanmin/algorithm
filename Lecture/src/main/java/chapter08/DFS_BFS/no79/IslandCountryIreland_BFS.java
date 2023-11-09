13d1582547d577a3616bf8988f95c6b62ce7d3ce
chanmin <behonestway@gmail.com>
2023-11-09 17:10:03 +0900

    79. 섬나라 아일랜드(BFS)

--- Lecture/src/main/java/chapter08/DFS_BFS/no79/IslandCountryIreland_BFS.java
+++ Lecture/src/main/java/chapter08/DFS_BFS/no79/IslandCountryIreland_BFS.java
@@ -1,5 +1,7 @@
 package chapter08.DFS_BFS.no79;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class IslandCountryIreland_BFS {
@@ -7,14 +9,32 @@ public class IslandCountryIreland_BFS {
     private static int[][] world;
     private static int[] dx = {1, 0, -1, 0, 1, -1, 1, -1};
     private static int[] dy = {0, 1, 0, -1, -1, -1, 1, 1};
-    private static void DFS(int x, int y) {
-        world[x][y] = 0;
+    private static class Point {
+        int x;
+        int y;
 
-        for(int i=0; i<dx.length; i++) {
-            int mx = x + dx[i];
-            int my = y + dy[i];
-            if(mx >= 0 && mx < size && my >=0 && my < size && world[mx][my] == 1)
-                DFS(mx, my);
+        public Point(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+    private static void BFS(int x, int y) {
+        Queue<Point> Q = new LinkedList<>();
+        Q.add(new Point(x, y));
+
+        while(!Q.isEmpty()) {
+            int len = Q.size();
+            for(int i=0; i<len; i++) {
+                Point island = Q.poll();
+                world[island.x][island.y] = 0;
+
+                for(int j=0; j<dx.length; j++) {
+                    int mx = island.x + dx[j];
+                    int my = island.y + dy[j];
+                    if(mx>=0 && mx<size && my>=0 && my<size && world[mx][my]==1)
+                        Q.add(new Point(mx, my));
+                }
+            }
         }
     }
     private static int solution() {
@@ -23,7 +43,7 @@ public class IslandCountryIreland_BFS {
         for(int i=0; i<size; i++) {
             for(int j=0; j<size; j++) {
                 if(world[i][j] == 1) {
-                    DFS(i, j);
+                    BFS(i, j);
                     answer++;
                 }
             }
