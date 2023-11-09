20e1398db42c9350101593d578f51b183dde41b3
chanmin <behonestway@gmail.com>
2023-11-09 23:10:50 +0900

    81. 씨름 선수

--- Lecture/src/main/java/chapter09/GreedyAlgorithm/no81/WrestlingPlayer.java
+++ Lecture/src/main/java/chapter09/GreedyAlgorithm/no81/WrestlingPlayer.java
@@ -1,7 +1,51 @@
 package chapter09.GreedyAlgorithm.no81;
 
-public class WrestlingPlayer {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
+public class WrestlingPlayer {
+    public static class Man implements Comparable<Man> {
+        int h, w;
+
+        public Man(int h, int w) {
+            this.h = h;
+            this.w = w;
+        }
+
+        @Override
+        public int compareTo(Man o) {
+            if(h == o.h) return w - o.w;
+            else return h - o.h;
+        }
+    }
+    private static int solution(List<Man> list) {
+        Collections.sort(list, Collections.reverseOrder());
+        int answer = list.size();
+        int heavy = 0;
+
+        for(Man m : list) {
+            if(heavy == 0) heavy = m.w;
+            else {
+                if(m.w < heavy) answer--;
+                heavy = Math.max(heavy, m.w);
+            }
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Man> list = new ArrayList<>();
+        int n = sc.nextInt();
+
+        for(int i=0; i<n; i++) {
+            int h = sc.nextInt();
+            int w = sc.nextInt();
+            list.add(new Man(h, w));
+        }
+
+        System.out.println(solution(list));
     }
 }
