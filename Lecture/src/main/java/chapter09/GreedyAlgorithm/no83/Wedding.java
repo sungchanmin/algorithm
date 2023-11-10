027ae28eb7637c42ee7b3328ed14368d8b236b09
chanmin <behonestway@gmail.com>
2023-11-10 11:47:51 +0900

    83. 결혼식

--- Lecture/src/main/java/chapter09/GreedyAlgorithm/no83/Wedding.java
+++ Lecture/src/main/java/chapter09/GreedyAlgorithm/no83/Wedding.java
@@ -1,7 +1,31 @@
 package chapter09.GreedyAlgorithm.no83;
 
-public class Wedding {
-    public static void main(String[] args) {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Wedding {
+    private static int solution(int[] start, int[] end) {
+        int[] arr = new int[72];
+
+        for(int i=0; i<start.length; i++) {
+            for(int j=start[i]; j<end[i]; j++) {
+                arr[j]++;
+            }
+        }
+
+        return Arrays.stream(arr).max().getAsInt();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] start = new int[n];
+        int[] end = new int[n];
+
+        for(int i=0; i<n; i++) {
+            start[i] = sc.nextInt();
+            end[i] = sc.nextInt();
+        }
+
+        System.out.println(solution(start, end));
     }
 }
