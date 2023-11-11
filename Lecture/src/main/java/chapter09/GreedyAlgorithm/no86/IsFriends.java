827ea211b71d20c8051bde46126ff983dcea48ff
chanmin <behonestway@gmail.com>
2023-11-11 14:25:23 +0900

    86. 친구인가

--- Lecture/src/main/java/chapter09/GreedyAlgorithm/no86/IsFriends.java
+++ Lecture/src/main/java/chapter09/GreedyAlgorithm/no86/IsFriends.java
@@ -1,7 +1,43 @@
 package chapter09.GreedyAlgorithm.no86;
 
-public class IsFriends {
-    public static void main(String[] args) {
+import java.util.*;
 
+public class IsFriends {
+    private static List<List<Integer>> relation = new ArrayList<>();
+    private static int[] ch;
+    private static int n, m;
+
+    private static String solution(int a, int b) {
+        Queue<Integer> Q = new LinkedList();
+        Q.add(a);
+
+        while(!Q.isEmpty()) {
+            int man = Q.poll();
+            ch[man] = 1;
+
+            for(int x : relation.get(man)) {
+                if(x == b) return "YES";
+                if(ch[x] == 0) Q.add(x);
+            }
+        }
+
+        return "NO";
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        ch = new int[n+1];
+
+        for(int i=0; i<=n; i++) relation.add(new ArrayList<>());
+
+        for(int i=0; i<m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            relation.get(a).add(b);
+            relation.get(b).add(a);
+        }
+
+        System.out.println(solution(sc.nextInt(), sc.nextInt()));
     }
 }
