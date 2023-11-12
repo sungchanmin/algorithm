1aad17c9eeb8dd85119ff4348995c0592186ca39
chanmin <behonestway@gmail.com>
2023-11-12 21:15:32 +0900

    89. 계단 오르기

--- Lecture/src/main/java/chapter10/DynamicProgramming/no89/ClimbingStairs.java
+++ Lecture/src/main/java/chapter10/DynamicProgramming/no89/ClimbingStairs.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class ClimbingStairs {
     private static int solution(int n) {
-        int[] arr = new int[n + 1];
-        arr[1] = 1;
-        arr[2] = 2;
+        int[] arr = new int[n];
+        arr[0] = 1;
+        arr[1] = 2;
 
-        for(int i=3; i<=n; i++) {
+        for(int i=2; i<n; i++) {
             arr[i] = arr[i-2] + arr[i-1];
         }
 
-        return arr[n];
+        return arr[n-1];
     }
 
     public static void main(String[] args) {
