655398c333c2cbd93b457ebee7489c44a468f091
chanmin <behonestway@gmail.com>
2023-11-12 21:24:18 +0900

    90. 돌다리 건너기

--- Lecture/src/main/java/chapter10/DynamicProgramming/no90/CrossingStoneBridge.java
+++ Lecture/src/main/java/chapter10/DynamicProgramming/no90/CrossingStoneBridge.java
@@ -1,7 +1,23 @@
 package chapter10.DynamicProgramming.no90;
 
-public class CrossingStoneBridge {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class CrossingStoneBridge {
+    private static int solution(int n) {
+        int[] arr = new int[n+1];
+        arr[0] = 1;
+        arr[1] = 2;
+
+        for(int i=2; i<n; i++) {
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+
+        return arr[n-1];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(solution(n + 1));
     }
 }
