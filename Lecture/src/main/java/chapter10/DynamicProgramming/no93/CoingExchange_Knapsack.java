364b7a3fd7254bfa1b2d362eb55d14f0d9750eec
chanmin <behonestway@gmail.com>
2023-11-13 15:20:33 +0900

    93. 동전 교환(냅색 알고리즘)

--- Lecture/src/main/java/chapter10/DynamicProgramming/no93/CoingExchange_Knapsack.java
+++ Lecture/src/main/java/chapter10/DynamicProgramming/no93/CoingExchange_Knapsack.java
@@ -1,7 +1,32 @@
 package chapter10.DynamicProgramming.no93;
 
-public class CoingExchange_Knapsack {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class CoingExchange_Knapsack {
+    private static int solution(int[] coins, int change) {
+       int[] dy = new int[change + 1];
+
+       for(int i=0; i<=change; i++) dy[i] = i;
+
+       for(int i=1; i<coins.length; i++) {
+           int coin = coins[i];
+
+           for(int j=coin; j<=change; j++) {
+               dy[j] = Math.min(dy[j], dy[j-coin] + 1);
+           }
+       }
+
+       return dy[change];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] coins = new int[n];
+
+        for(int i=0; i<n; i++) coins[i] = sc.nextInt();
+
+        int change = sc.nextInt();
+        System.out.println(solution(coins, change));
     }
 }
