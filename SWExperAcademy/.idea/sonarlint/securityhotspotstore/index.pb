bfe515376bf3b0301cabfd09b332b190cde9c5dd
chanmin <behonestway@gmail.com>
2023-11-14 21:13:58 +0900

    1859. 백만 장자 프로젝트

---
+++ SWExperAcademy/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,15 @@
+
+S
+#.idea/sonarlint/issuestore/index.pb,9/f/9fe84ebb15faf917b7def6236dba604453cc61e0
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8/8/88a9255124c95bdc913197c120a8d560edc59c8e
+K
+src/main/java/D1/P2029.java,0/6/067ba592593cd814ce8a5d5de5d3eecdbc4f1a41
+K
+src/main/java/D1/P2056.java,7/1/71537f253cf14792460b3c0df506f3f70f78fc38
+K
+src/main/java/D2/P1859.java,f/5/f5b36b1cc157bd15fb7e15677cfd1752884074f3
+K
+src/main/java/Solution.java,2/6/26b1745296c4730ead1a40cbba1b589e8ef778ae
---
+++ SWExperAcademy/src/main/java/Solution.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Solution {
+    // 1859. 백만 장자 프로젝트
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n; i++) {
+            int m = sc.nextInt();
+            int[] arr = new int[m];
+
+            for(int j=0; j<m; j++) arr[j] = sc.nextInt();
+
+            System.out.println("#" + i + " " + solution(arr));
+        }
+    }
+
+    private static long solution(int[] arr) {
+        long answer = 0;
+        int max = 0;
+
+        for(int i=arr.length-1; i>=0; i--) {
+            max = Math.max(max, arr[i]);
+            answer += max - arr[i];
+        }
+
+        return answer;
+    }
+}
+
+/**
+ *  10개중 7개 맞았다고 하시는 분들
+ * 타입이 틀린겁니다.
+ * 문제를 잘 읽어보세요
+ * TC가 100만에 최대 매매가는 10000입니다.
+ * 만약 마지막날 빼고 전부다 1원이라고 가정을 해보세요.
+ * 계산기 두들겨 보면 99억 넘게 나옵니다...
+ * int타입으로 표현 가능한 숫자는 21억 정도예요.
+ * 그럼 표현할수 있는 범위를 벗어나서 오답처리가 되는겁니다.
+ * long타입으로 바꾸세요.
+ */
