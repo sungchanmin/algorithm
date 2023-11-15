f0e7678412ee4131ea4648c40261816865164184
chanmin <behonestway@gmail.com>
2023-11-15 13:05:41 +0900

    1928. Base64 Decoder

--- SWExperAcademy/src/main/java/D2/P1928_2.java
+++ SWExperAcademy/src/main/java/D2/P1928_2.java
@@ -1,7 +1,9 @@
+package D2;
+
 import java.util.Base64;
 import java.util.Scanner;
 
-public class Solution {
+public class P1928_2 {
     // 1928. Base64 Decoder
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
