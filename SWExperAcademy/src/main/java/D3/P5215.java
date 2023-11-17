5e3ce8a4f45a1aca643940e9fd059709ba678ba1
chanmin <behonestway@gmail.com>
2023-11-17 17:47:13 +0900

    5215. 햄버거 다이어트

---
+++ SWExperAcademy/src/main/java/D3/P5215.java
@@ -0,0 +1,47 @@
+package D3;
+
+import java.util.Scanner;
+
+public class P5215 {
+    // 5215. 햄버거 다이어트
+    private static int n, max;
+    private static int[] happy, kcal;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = Integer.parseInt(sc.nextLine());
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+            n = sc.nextInt();
+            max = sc.nextInt();
+
+            happy = new int[n];
+            kcal = new int[n];
+
+            for(int i=0; i<n; i++) {
+                happy[i] = sc.nextInt();
+                kcal[i] = sc.nextInt();
+            }
+
+
+            System.out.println("#" + test_case + " " + DFS(0, 0, 0));
+        }
+    }
+
+    private static int DFS(int i, int sumKcal, int sumHappy) {
+        if(i >= n) return sumHappy;
+        int a = 0, b = 0;
+
+        sumKcal += kcal[i];
+        sumHappy += happy[i];
+        if(sumKcal <= max) {
+            a = DFS(i + 1, sumKcal, sumHappy);
+        }
+
+        sumKcal -= kcal[i];
+        sumHappy -= happy[i];
+        b = DFS(i + 1, sumKcal, sumHappy);
+
+        return Math.max(sumHappy, Math.max(a, b));
+    }
+}
