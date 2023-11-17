d97637f7327028e2add9eba43a47640b54a54888
chanmin <behonestway@gmail.com>
2023-11-17 17:47:50 +0900

    5215. 햄버거 다이어트

--- SWExperAcademy/src/main/java/Solution.java
+++ SWExperAcademy/src/main/java/Solution.java
@@ -1,22 +1,45 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class Solution{
+        public class Solution {
+            // 5215. 햄버거 다이어트
+            private static int n, max;
+            private static int[] happy, kcal;
 
-    public static void main(String args[]){
+            public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+                int T = Integer.parseInt(sc.nextLine());
 
-        Scanner input = new Scanner(System.in);
-        int a = input.nextInt();
+                for (int test_case = 1; test_case <= T; test_case++) {
+                    n = sc.nextInt();
+                    max = sc.nextInt();
 
-        for(int b=1; b<=a; b++) {
-            float sum = 0;
-            for(int c=1; c<=10; c++) {
-                int d = input.nextInt();
-                sum += d;
+                    happy = new int[n];
+                    kcal = new int[n];
+
+                    for(int i=0; i<n; i++) {
+                        happy[i] = sc.nextInt();
+                        kcal[i] = sc.nextInt();
+                    }
+
+
+                    System.out.println("#" + test_case + " " + DFS(0, 0, 0));
+                }
             }
-            System.out.println("#" +a+ " " + String.format("%.1f", sum / 10));
-        }
 
+            private static int DFS(int i, int sumKcal, int sumHappy) {
+                if(i >= n) return sumHappy;
+                int a = 0, b = 0;
 
-    }
+                sumKcal += kcal[i];
+                sumHappy += happy[i];
+                if(sumKcal <= max) {
+                    a = DFS(i + 1, sumKcal, sumHappy);
+                }
 
+                sumKcal -= kcal[i];
+                sumHappy -= happy[i];
+                b = DFS(i + 1, sumKcal, sumHappy);
+
+                return Math.max(sumHappy, Math.max(a, b));
+            }
 }
