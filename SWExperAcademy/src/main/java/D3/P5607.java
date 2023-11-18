701f12a4ee41c39314a84e73e0dc8f764c490386
chanmin <behonestway@gmail.com>
2023-11-18 16:38:17 +0900

    5607. 조합

--- SWExperAcademy/src/main/java/D3/P5607.java
+++ SWExperAcademy/src/main/java/D3/P5607.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class P5607 {
     // 5607. 조합
     private static final long num = 1234567891;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
@@ -13,25 +14,39 @@ public class P5607 {
             int n = sc.nextInt();
             int k = sc.nextInt();
 
-            System.out.println("#" + test_case + " " + clac(n, k));
+            System.out.println("#" + test_case + " " + calc(n, k));
         }
     }
 
-    private static long clac(int n, int k) {
-        long p = 1;
-        long c = 1;
+    private static long calc(int n, int k) {
+        long numerator = 1;
+        long denominator = 1;
 
-        for(int i=n; i>n-k; i--) {
-            p *= i;
-            p %= num;
+        for (int i = n; i > n - k; i--) {
+            numerator = (numerator * i) % num;
         }
 
-        for(int i=k; i>1; i--) {
-            c *= i;
-            c %= num;
+        for (int i = k; i > 1; i--) {
+            denominator = (denominator * i) % num;
         }
 
-        return p/c;
+        // 분모의 역원을 구해서 나누기를 수행
+        long result = (numerator * power(denominator, num - 2)) % num;
+        return result;
     }
 
+    private static long power(long base, long exponent) {
+        if (exponent == 0) {
+            return 1;
+        }
+
+        long halfPower = power(base, exponent / 2) % num;
+        long result = (halfPower * halfPower) % num;
+
+        if (exponent % 2 == 1) {
+            result = (result * base) % num;
+        }
+
+        return result;
+    }
 }
