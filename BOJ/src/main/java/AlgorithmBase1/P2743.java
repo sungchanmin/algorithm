64c59a4abfb87cccffeb8adc6585b485802246df
chanmin <behonestway@gmail.com>
2023-12-12 10:50:54 +0900

    18. 단어 길이 재기

---
+++ BOJ/src/main/java/AlgorithmBase1/P2743.java
@@ -0,0 +1,13 @@
+package AlgorithmBase1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P2743 {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println(br.readLine().length());
+        }
+    }
+}
