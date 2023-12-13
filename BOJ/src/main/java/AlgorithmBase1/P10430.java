c4213d4c3b44f05a62ee98192e001bb5b2174935
chanmin <behonestway@gmail.com>
2023-12-13 18:18:05 +0900

    22. 나머지

---
+++ BOJ/src/main/java/AlgorithmBase1/P10430.java
@@ -0,0 +1,19 @@
+package AlgorithmBase1;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class P10430 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C) * (B%C))%C);
+    }
+}
