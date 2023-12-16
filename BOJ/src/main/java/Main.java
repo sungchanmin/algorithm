5d74a4e0c5c503609321942fa417447288ce7fa3
chanmin <behonestway@gmail.com>
2023-12-16 19:54:26 +0900

    25. 소수 찾기

--- BOJ/src/main/java/Main.java
+++ BOJ/src/main/java/Main.java
@@ -1,41 +1,39 @@
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Stack;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
-            int n = Integer.parseInt(br.readLine());
-            int[] arr = new int[n];
+        Scanner sc =  new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        int[] prime = new int[1001];
+        String[] arr = sc.nextLine().split(" ");
 
-            Map<Integer, Integer> map = new HashMap<>();
-            Stack<Integer> stack = new Stack<>();
+        getPrime(prime);
+        System.out.println(countPrime(arr, prime));
+    }
 
-            String[] strArr = br.readLine().split(" ");
+    private static void getPrime(int[] arr) {
+        arr[1] = 1;
 
-            for(int i=0; i<n; i++) {
-                arr[i] = Integer.parseInt(strArr[i]);
-                map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
-            }
+        for(int i=2; i<arr.length; i++) {
+            if(arr[i] == 0) {
+                int n = i * 2;
 
-            for(int i=0; i<n; i++) {
-                while(!stack.isEmpty()) {
-                    if(map.get(arr[stack.peek()]) < map.get(arr[i]) && arr[stack.peek()] != arr[i]) {
-                        arr[stack.pop()] = arr[i];
-                    } else break;
+                while(n < arr.length) {
+                    arr[n] = 1;
+                    n += i;
                 }
-                stack.push(i);
             }
-
-            while(!stack.isEmpty()) {
-                arr[stack.pop()] = -1;
-            }
-
-            for(int i : arr) bw.write(i + " ");
-
-            bw.flush();
         }
+    }
+
+    private static int countPrime(String[] arr, int[] prime) {
+        int cnt = 0;
+
+        for(String s : arr) {
+            if(prime[Integer.parseInt(s)] == 0) cnt++;
+        }
+
+        return cnt;
     }
 }
