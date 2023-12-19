82a36a8f0070ddc7a7723d5fe98fe1564256ca35
chanmin <behonestway@gmail.com>
2023-12-19 14:33:23 +0900

    32. 2진수 8진수

---
+++ BOJ/src/main/java/AlgorithmBase1/P1373.java
@@ -0,0 +1,16 @@
+package AlgorithmBase1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class P1373 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
+        BigInteger N = new BigInteger(S, 2);
+        String result = N.toString(8);
+        System.out.println(result);
+    }
+}
