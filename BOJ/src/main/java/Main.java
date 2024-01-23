5e3c0c84ec6217ede30d2971b253e38cbbd0c6fb
admin <admin@10.64.10.153>
2024-01-23 17:47:28 +0900

    test

--- BOJ/src/main/java/Main.java
+++ BOJ/src/main/java/Main.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // commit test
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	String str = br.readLine();
+    	System.out.println(str);
     }
 }
