ca0209c1e15038855b081b1712bd92940201817e
admin <admin@10.64.10.153>
2024-01-23 17:23:55 +0900

    commit test

--- BOJ/src/main/java/Main.java
+++ BOJ/src/main/java/Main.java
@@ -5,24 +5,6 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-
-        int[] arr = new int[N];
-        int[] dp = new int[N];
-
-        String[] strArr = br.readLine().split(" ");
-
-        for(int i=0; i<N; i++) arr[i] = Integer.parseInt(strArr[i]);
-
-        Arrays.fill(dp, 1);
-
-        for(int i=1; i<N; i++) {
-            for(int j=0; j<i; j++) {
-                if(arr[j] > arr[i]) dp[i] = Math.max(dp[i], dp[j] + 1);
-            }
-        }
-
-        System.out.println(Arrays.stream(dp).max().getAsInt());
+        // commit test
     }
 }
