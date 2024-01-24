7bfac19e4fc63edbbf39f3d4e41ca12c204e26d2
chanmin <adorableyou26@gmail.com>
2024-01-24 17:33:42 +0900

    67. RGB거리 2

--- BOJ/src/main/java/AlgorithmBase1/P17404.java
+++ BOJ/src/main/java/AlgorithmBase1/P17404.java
@@ -6,57 +6,53 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class P17404 {
-    static final int INF = 987654321;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static int solution(int n, int[][] rgbs) {
-        //최대 비용으로 초기화
-        int answer = INF;
-        //비용을 저장할 dp, i번째 집을 r/g/b로 칠할 경우, 최소 비용을 저장
-        int[][] dp = new int[n][3];
+		int answer = Integer.MAX_VALUE;
+		int N = Integer.parseInt(br.readLine());
+		int[][] arr = new int[N][3];
+		
+		for (int i = 0; i < N; i++)
+			arr[i] = toArr(br.readLine());
+		
+		// 처음 집을 빨강, 초록, 파랑 중 한 가지 색을 칠하는 경우로 나누어 모두 확인한다.
+		for (int n = 0; n < 3; n++) {
+			int[][] dp = new int[N][3];
+			
+			// 처음 경우의 수를 모두 한 가지(n번째)색으로 지정한다.
+			dp[0][0] = arr[0][n];
+			dp[0][1] = arr[0][n];
+			dp[0][2] = arr[0][n];
+			
+			// 다음 집부터 앞 집과 색이 겹치지 않으면서, 최소값이 되는 경우를 선택한다.
+			for (int i = 1; i < N; i++) {
+				dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
+				dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
+				dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
+				
+				// 단, 두 번째 집을 칠하는 경우에서 첫 번째 집에 지정한 색과 겹치는 경우의 수를 배제한다. (최대값 부여)
+				if(i==1) dp[i][n] = Integer.MAX_VALUE;
+			}
+			
+			// 마지막 집을 고르는 경우 중 최소 값을 찾는다.
+			for (int i = 0; i < 3; i++) {
+				// 단, 첫 번째 집에 지정한 색을 고른 경우를 배제한다. (조건문 : i != n)
+				if (i != n) answer = Math.min(answer, dp[N - 1][i]);
+			}
+		}
+	
+		System.out.println(answer);
 
-        //i : 처음 색을 지정
-        for(int i=0;i<3;i++) {
-            //처음 i번째 색을 선택하기 위해, i번째 색 이외 INF로 초기화
-            for(int j=0;j<3;j++) {
-                dp[0][j]=i==j?rgbs[0][i]:INF;
-            }
-            //집 탐색
-            for(int j=1;j<n;j++) {
-                //이전 집과 다른 색을 칠하는 경우 중 최소값을 저장
-                for(int k=0;k<3;k++) {
-                    dp[j][k]=INF;
-                    for(int l=0;l<3;l++) {
-                        if(k==l) continue;
-                        dp[j][k]=Math.min(dp[j][k], dp[j-1][l]+rgbs[j][k]);
-                    }
-                }
-            }
-            //처음과 마지막집의 색이 다르면 최소값을 answer로 가져오기
-            for(int j=0;j<3;j++) {
-                if(i==j) continue;
-                answer=Math.min(answer, dp[n-1][j]);
-            }
-        }
-        //출력
-        return answer;
-    }
+	}
 
+	private static int[] toArr(String str) {
+		String[] strArr = str.split(" ");
+		int[] arr = new int[strArr.length];
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine()); //집의 개수
-        StringTokenizer st;
-        int[][] rgbs = new int[n][3]; //각 색을 칠하는 비용
-        //비용 입력
-        for(int i=0;i<n;i++) {
-            st=new StringTokenizer(br.readLine());
-            rgbs[i][0]=Integer.parseInt(st.nextToken());
-            rgbs[i][1]=Integer.parseInt(st.nextToken());
-            rgbs[i][2]=Integer.parseInt(st.nextToken());
-        }
-        //최소 비용 출력
-        System.out.println(solution(n, rgbs));
+		for (int i = 0; i < arr.length; i++)
+			arr[i] = Integer.parseInt(strArr[i]);
 
-        br.close();
-    }
+		return arr;
+	}
 }
