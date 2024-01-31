f8cb02586770194d11da8d71add466404ea7a3ad
chanmin <adorableyou26@gmail.com>
2024-01-31 10:35:53 +0900

    07. 수 이어 쓰기 1

---
+++ BOJ/src/main/java/AlgorithmBase2/P1748.java
@@ -0,0 +1,26 @@
+package AlgorithmBase2;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P1748 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int mod = 10;
+		int len = 1;
+		int cnt = 0;
+
+		for(int num=1; num<=N; num++) {
+			// 자릿수가 바뀌지는 경우를 체크
+			if(num % mod == 0) {
+				len++;
+				mod *= 10;
+			}
+			cnt += len;
+		}
+		
+		System.out.println(cnt);
+	}
+}
