a8c2f58106c6b2c44523c295222eaa83b039b519
chanmin <adorableyou26@gmail.com>
2024-02-01 14:53:40 +0900

    09. N과 M (1)

--- BOJ/src/main/java/AlgorithmBase2/P15649.java
+++ BOJ/src/main/java/AlgorithmBase2/P15649.java
@@ -4,33 +4,35 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+
 public class P15649 {
+	//BOJ 1107 리모컨
 	private static int N, M;
 	private static boolean[] ch;
 	private static StringBuilder sb = new StringBuilder();
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] strArr = br.readLine().split(" ");
-		
+
 		N = Integer.parseInt(strArr[0]);
 		M = Integer.parseInt(strArr[1]);
 		ch = new boolean[N + 1];
-		
+
 		solution(M, "");
 		System.out.println(sb);
 	}
-	
+
 	private static void solution(int len, String str) {
-		if(len == 0) {
+		if (len == 0) {
 			sb.append(str).append("\n");
 			return;
 		}
-		
-		for(int i=1; i<=N; i++) {
-			if(ch[i] == false) {
+
+		for (int i = 1; i <= N; i++) {
+			if (ch[i] == false) {
 				ch[i] = true;
-				solution(len-1, str + i + " ");
+				solution(len - 1, str + i + " ");
 				ch[i] = false;
 			}
 		}
