ba1414a93cfd817e4be9a59405305a696bd41309
chanmin <adorableyou26@gmail.com>
2024-02-02 13:44:36 +0900

    13. N과 M (6)

---
+++ BOJ/src/main/java/AlgorithmBase2/P15654.java
@@ -0,0 +1,46 @@
+package AlgorithmBase2;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class P15654 {
+		//BOJ 15654 N과 M (5)
+		private static int N, M;
+		private static int[] arr;
+		private static boolean[] ch;
+		private static StringBuilder sb = new StringBuilder();
+
+		public static void main(String[] args) throws IOException {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			String[] strArr = br.readLine().split(" ");
+
+			N = Integer.parseInt(strArr[0]);
+			M = Integer.parseInt(strArr[1]);
+			
+			arr = new int[N + 1];
+			ch = new boolean[N + 1];
+			strArr = br.readLine().split(" ");
+			
+			for(int i=1; i<=N; i++) arr[i] = Integer.parseInt(strArr[i - 1]);
+			Arrays.sort(arr);
+			
+			solution(1, M, "");
+			System.out.println(sb);
+		}
+
+		private static void solution(int n, int len, String str) {
+			if (len == 0) {
+				sb.append(str).append("\n");
+				return;
+			}
+
+			for (int i = 1; i <= N; i++) {
+				if(ch[i] == false) {
+					ch[i] = true;
+					solution(i, len - 1, str + arr[i] + " ");
+					ch[i] = false;
+				}
+			}
+		}
+}
